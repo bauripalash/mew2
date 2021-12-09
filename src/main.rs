90c8b7a54bac09b6669ab2d67cabdeec4fcc4b30
Palash Bauri <palashbauri1@gmail.com>
2021-12-09 20:02:47 +0530

    implementing Scanner #1

--- src/main.rs
+++ src/main.rs
@@ -1,142 +1,140 @@
-use std::{fs, str::Chars, char, error::Error};
+#![allow(dead_code)]
+use std::fs;
 
-
-
-// fn read_file(f_path : &String) -> String{
-    // //println!("{}" , f_path);
-    // let f_contents = String::new();
-    // let _file = fs::read_to_string(f_path).unwrap();
-    // return f_contents;
-// }
-
-// fn iter_string(s : String){
-    // println!("{}" , s);
-    // //for item in {
-    // //    println!("=> {}" , item);
-    // //}
-
-// }
-
-// enum TokenType{
-    // ID,
-    // INTEGER
-// }
-
-// struct Token{
-    
-    // ttype : String,
-    // lexeme : String,
-    // line : u32
-
-// }
-
-// impl Token{
-    // fn to_string(&self) -> String{
-        // let s = String::from(format!("[Token][Type => {} ; Lexeme => '{}' ; Line => {}]" , self.ttype , self.lexeme , self.line));
-        // return s;
-    // }
-// }
-
-
-// struct Scanner{
-    
-    // source : String,
-    // tokens : Vec<Token>,
-    // start : u32,
-    // current : u32,
-    // line : u32 
-
-// }
-
-// impl Scanner{
-    // fn is_at_end(&self) -> bool{
-        // return self.current >= self.source.len() as u32; 
-    // }
-    // fn scan_token(&mut self){
-        // while !self.is_at_end(){
-            // self.start = self.current;
-            // self.scan_token();
-        // }
-        
-        // self.tokens.push(Token{ ttype : String::from("EOF") , lexeme : String::from("") , line : 1 });
-    // }
-    // fn pnt(&self){
-        // for item in self.source.chars(){
-            // println!("{}" , item.to_string())
-        // }
-    // }
-// }
-
-struct Token{
-    lexeme : String,
-    pos : u32,
-    line : u32
+struct Token {
+    lexeme: String,
+    pos: u32,
+    line: u32,
 }
 
-fn lex(c : String){
-//    let mut cx : Vec<Chars> = vec![*c];
-    // let length = c.len();
-    // let mut current : u32 = 0;
-    // let mut start : u32 = 0;
-    // let mut line : u32 = 1;
-//     let mut s_obj : Scanner = Scanner {
-        // source : c,
-        // tokens : Vec::new(),
-        // start : 0,
-        // current : 0,
-        // line : 1
+fn new_token(lexeme: String, pos: u32, line: u32) -> Token {
+    let _t: Token = Token { lexeme, pos, line };
+
+    return _t;
+}
+
+struct Scanner {
+    c_list: Vec<char>,
+    line: u32,
+    current: usize,
+    tok_list: Vec<Token>,
+}
+
+impl Scanner {
+    fn c_advance(mut self) -> char {
+        self.current += 1;
+
+        return self.c_list[self.current - 1];
+    }
+    fn at_eof(&self) -> bool {
+        return self.current >= self.c_list.len();
+    }
+    //TODO : Complete this function
+    //Donot use the print_token
+    fn scan_token(mut self) {
+        while !self.at_eof() {
+            let _vcur = self.c_list[self.current];
+            if !(_vcur == ' ') {
+                if _vcur == '\n' {
+                    self.line += 1;
+                    self.current += 1;
+                    continue;
+                }
+
+                let _token: Token = Token {
+                    lexeme: String::from(_vcur),
+                    pos: self.current as u32,
+                    line: self.line,
+                };
+                self.tok_list.push(_token);
+            }
+            self.current += 1;
+        }
+        println!("Scan Token");
+    }
+    //TODO : Just for testing and/or debugging
+    fn print_token(mut self) {
+        while !self.at_eof() {
+            let _vcur = self.c_list[self.current];
+            if !(_vcur == ' ') {
+                if _vcur == '\n' {
+                    self.line += 1;
+                    self.current += 1;
+                    continue;
+                }
+                match &_vcur {
+                    '=' => self.tok_list.push(new_token(
+                        String::from("EQ"),
+                        self.current as u32,
+                        self.line,
+                    )),
+                    ';' => self.tok_list.push(new_token(
+                        String::from("SQ"),
+                        self.current as u32,
+                        self.line,
+                    )),
+                    _ => self.tok_list.push(new_token(
+                        String::from(_vcur),
+                        self.current as u32,
+                        self.line,
+                    )),
+                }
+            }
+            self.current += 1;
+        }
+        println!("Length => {}", self.tok_list.len());
+        for item in &self.tok_list {
+            println!(
+                "[Token ~> {} ~> {} ~> {}]",
+                item.lexeme, item.pos, item.line
+            );
+        }
+    }
+}
+
+fn lex(c: String) {
+    //     let mut current: u32 = 0;
+    // let mut line: u32 = 1;
+    // let mut token_list: Vec<Token> = Vec::new();
+    // let _vc = c.chars().collect::<Vec<char>>();
+    // while !(current >= _vc.len() as u32) {
+    // let vcur = _vc[current as usize];
+    // if !(vcur == ' ') {
+    // if vcur == '\n' {
+    // line += 1;
+    // current += 1;
+    // continue;
+    // }
+    // let _token: Token = Token {
+    // lexeme: String::from(vcur),
+    // pos: current,
+    // line,
     // };
-    // s_obj.scan_token();
-//     s_obj.pnt();
-    let mut current : u32 = 0;
-    let mut line : u32 = 1;
-    let mut token_list : Vec<Token> = Vec::new();
-    let _vc = c.chars().collect::<Vec<char>>();
-     while !(current >= _vc.len() as u32) {
-         let vcur = _vc[current as usize];
-        // if c[current as usize] != ' ' || c[current as usize] != '\n'{
-        // println!("=> {} <=" , c[current as usize]);
-        // current+=1
-        if !(vcur ==  ' '){
-        if vcur == '\n'{
-           line += 1; 
-           current+=1;
-           continue;
-        }
-        let _token : Token = Token{
-            lexeme : String::from(vcur),
-            pos : current,
-            line
-        };
 
-        token_list.push(_token);
+    // token_list.push(_token);
+    // }
+    // current += 1;
+    // }
 
-        // println!("{:?}" , _token);
-        
-        
-        
-        }
-        current += 1;
-        
-     }
-
-     println!("+ Length => {}" , token_list.len());
-     for item in &token_list{
-         println!("[Token ~> {} ~> {} ~> {}]" , item.lexeme , item.pos , item.line);
-     }
-
+    // println!("+ Length => {}", token_list.len());
+    // for item in &token_list {
+    // println!(
+    // "[Token ~> {} ~> {} ~> {}]",
+    // item.lexeme, item.pos, item.line
+    // );
+    // }
+    let m_scanner: Scanner = Scanner {
+        c_list: c.chars().collect::<Vec<char>>(),
+        line: 1,
+        current: 0,
+        tok_list: Vec::new(),
+    };
+    m_scanner.print_token();
 }
 
 fn main() {
     let _f = String::from("/home/palash/Desktop/rol/c/src/a.txt");
 
     let _fc = fs::read_to_string(_f).unwrap();
-    // let ic = _fc.chars().collect::<Vec<char>>();
     lex(_fc);
-    //for item in ic{
-    //    println!("{}" , item);
-    //}
-    // println!("{}" , _fc);
-    // iter_string(_fc);
-
 }
